2ab982458e552e53a078fe33ac48f9397c928efe
bahaa dabbagh <bahaa.dabbagh@gmail.com>
2020-05-18 14:16:34 +0000

    Automatically backed up by Learn

--- src/Comment.js
+++ src/Comment.js
@@ -1,1 +1,10 @@
-//your code here
+// your code here
+import React, { Component } from 'react';
+
+export default class Comment extends Component {
+    render() {
+        return (
+            <div className="Comment">{this.props.commentText}</div>
+        );
+    }
+}
